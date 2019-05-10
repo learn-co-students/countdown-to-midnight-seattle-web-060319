be6d7e013998e16ee4bfe98d92bb886b4db83f6e
Dan-Vy Le <danvyle85@gmail.com>
2019-05-10 00:16:54 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,28 @@
 #write your code here
+# count = 12
 
-def countdown
+# count = 12
+#write your code here
+# count = 12
+
+def countdown(count)
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(count)
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+
+
+
+ 
